c6a79efca1b06c409a8f7e33225b7c8ef59aa49d
Caique Nunes <caiquen189@gmail.com>
2025-02-25 13:55:02 -0300

    feat: first commit

---
+++ src/main/java/com/silvestre_lanchonete/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.silvestre_lanchonete.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+		System.out.println("HTTP Server running on port: 8080");
+	}
+
+}
