5780f8e315b3904d5a87b9bc4999421dd0fb28a7
Caique Nunes <caiquen189@gmail.com>
2025-03-14 14:12:38 -0300

    feat: application configuration

---
+++ src/main/java/com/silvestre_lanchonete/api/infra/security/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.silvestre_lanchonete.api.infra.security;
+
+import com.silvestre_lanchonete.api.infra.cors.CorsConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    SecurityFilter securityFilter;
+
+    @Autowired
+    CorsConfig corsConfig;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/api-docs/**"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/admin/products").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/orders").hasAnyAuthority("Administrador", "Usuario")
+                        .requestMatchers(HttpMethod.GET, "/orders").hasAnyAuthority("Administrador", "Usuario")
+                        .requestMatchers(HttpMethod.PUT, "/orders/**").hasAuthority("Administrador")
+                        .requestMatchers("/admin/**").hasAnyAuthority("Administrador")
+                        .anyRequest().authenticated()
+                )
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/silvestre_lanchonete/api/infra/websocket/OrderStatusWebSocketHandler.java
@@ -0,0 +1,33 @@
+package com.silvestre_lanchonete.api.infra.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class OrderStatusWebSocketHandler extends TextWebSocketHandler {
+
+    private static final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {}
+
+    public void sendNotification(String message) {
+        for (WebSocketSession session : sessions) {
+            try {
+                session.sendMessage(new TextMessage(message));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
